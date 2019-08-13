aac71e15d4c4861a556250b8ec6953179c837ba4
amokgohl <mokgohloallie@gmail.com>
2019-08-13 15:12:12 +0200

    tests

--- spec/passwordCheckerSpec.js
+++ spec/passwordCheckerSpec.js
@@ -1,30 +1,35 @@
 describe("Password validator", ()=>{
     it ("Returns error if the password length is less than 8 characters", ()=> {
-        
-        expect(parser.parse(raw)).toThrow(new Error("Parsing is not possible"));
+        var raw;
+        var answer = password_is_valid(raw);
+        expect(parser.parse(answer)).toThrow(new Error("Password must contain 8 characters"));
     })
 })
 describe("Password validator", ()=>{
-    it ("Returns error if the password does not consist atleast 1 capital letter", ()=> {
-        
-        expect(parser.parse(raw)).toThrow(new Error("Parsing is not possible"));
+    it ("Returns error if the password does not consist atleast 1 upper case and 1 lower case", ()=> {
+        var raw;
+        var answer = password_is_valid(raw);
+        expect(parser.parse(answer)).toThrow(new Error("Password must contain 1 uppercase annd lower case character"));
     })
 })
 describe("Password validator", ()=>{
     it ("Returns error if the password does not consist atleast 1 number", ()=> {
-        
-        expect(parser.parse(raw)).toThrow(new Error("Parsing is not possible"));
+        var raw;
+        var answer = password_is_valid(raw);
+        expect(parser.parse(answer)).toThrow(new Error("Password must contain Numbers"));
     })
 })
 describe("Password validator", ()=>{
     it ("Returns error if the password does not consist special characters", ()=> {
-        
-        expect(parser.parse(raw)).toThrow(new Error("Parsing is not possible"));
+        var raw;
+        var answer = password_is_valid(raw);
+        expect(parser.parse(answer)).toThrow(new Error("Password must contain 1 special character"));
     })
 })
 describe("Password validator", ()=>{
-    it ("Returns error if the password does not consist small letters", ()=> {
-        
-        expect(parser.parse(raw)).toThrow(new Error("Parsing is not possible"));
+    it ("Returns error if the password field is empty", ()=> {
+        var raw;
+        var answer = password_is_valid(raw);
+        expect(parser.parse(answer)).toThrow(new Error("Password must not be null"));
     })
 })
---
+++ src/passwordChecker.js
@@ -0,0 +1,14 @@
+function password_is_valid(password)
+{
+    try {
+        if (/[A-Z][a-z]/){
+
+        }
+        if (/[0-9]/){
+
+        }
+    }
+    catch{
+        
+    }
+}
