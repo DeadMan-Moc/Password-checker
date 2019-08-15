85a192d4fcc29402ca43f2d680fdecce56e90e15
Alfred Mokgohloa <45788338+DeadMan-Moc@users.noreply.github.com>
2019-08-15 12:30:50 +0200

    lil problems with tdd

--- spec/passwordCheckerSpec.js
+++ spec/passwordCheckerSpec.js
@@ -1,35 +1,35 @@
+// describe("Password validator", ()=>{
+//     it ("Returns error if the password length is less than 8 characters", ()=> {
+//         var raw = "asdAAda489";
+//          var answer = 8;
+//         expect(password_is_valid(raw)).toBeGreaterthan(answer);
+//     })
+// })
 describe("Password validator", ()=>{
-    it ("Returns error if the password length is less than 8 characters", ()=> {
-        var raw;
+    it ("Checks for strong password.", ()=> {
+        var raw = "123OOPs$$5";
         var answer = password_is_valid(raw);
-        expect(parser.parse(answer)).toThrow(new Error("Password must contain 8 characters"));
+        expect(answer).toBe(true);
     })
 })
-describe("Password validator", ()=>{
-    it ("Returns error if the password does not consist atleast 1 upper case and 1 lower case", ()=> {
-        var raw;
-        var answer = password_is_valid(raw);
-        expect(parser.parse(answer)).toThrow(new Error("Password must contain 1 uppercase annd lower case character"));
-    })
-})
-describe("Password validator", ()=>{
-    it ("Returns error if the password does not consist atleast 1 number", ()=> {
-        var raw;
-        var answer = password_is_valid(raw);
-        expect(parser.parse(answer)).toThrow(new Error("Password must contain Numbers"));
-    })
-})
-describe("Password validator", ()=>{
-    it ("Returns error if the password does not consist special characters", ()=> {
-        var raw;
-        var answer = password_is_valid(raw);
-        expect(parser.parse(answer)).toThrow(new Error("Password must contain 1 special character"));
-    })
-})
-describe("Password validator", ()=>{
-    it ("Returns error if the password field is empty", ()=> {
-        var raw;
-        var answer = password_is_valid(raw);
-        expect(parser.parse(answer)).toThrow(new Error("Password must not be null"));
-    })
-})
+// describe("Password validator", ()=>{
+//     it ("Returns error if the password does not consist atleast 1 number", ()=> {
+//         var raw;
+//         var answer = password_is_valid(raw);
+//         expect(parser.parse(answer)).toThrow(new Error("Password must contain Numbers"));
+//     })
+// })
+// describe("Password validator", ()=>{
+//     it ("Returns error if the password does not consist special characters", ()=> {
+//         var raw;
+//         var answer = password_is_valid(raw);
+//         expect(parser.parse(answer)).toThrow(new Error("Password must contain 1 special character"));
+//     })
+// })
+// describe("Password validator", ()=>{
+//     it ("Returns error if the password field is empty", ()=> {
+//         var raw;
+//         var answer = password_is_valid(raw);
+//         expect(parser.parse(answer)).toThrow(new Error("Password must not be null"));
+//     })
+// })
--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -1,38 +1,37 @@
 function password_is_valid(password)
 {
-    try {
-        if (password == /[A-Z]|[a-z]/g){
-        }
-    }
-    catch{
+//     try {
+//        if (password !== password.match(/\d+/) ){
+//             // throw new Error("password should include 1 number")
+//         }
+//         if (password.match(/[!@#$%^&*-_(),.?":{}|<>]/g) ){
+//             // throw new Error("password should include 1 special character")
+//         }
+//         if (password.length !== 0) {
+//             // throw new Error("password cannot be empty")
+//         }
+//         if (password.length < 8){
+//             // throw new Error("password should be longer than 8 characters")
+//         } 
+//     }
+//     catch(err){
+//         throw new Error("Error "+err);
+//     }
+// }
 
-    }
-    try {
-        if (password == /[0-9]/g){
+        if (password.length != 0 && password.length >= 8)
+        {
+            if (password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])/)) {
+                throw ("password is valid")
+            }
+            else {
+                throw ("password is invalid indeed.")
+            }
+        }
+        else {
+            throw("password is invalid")
         }
     }
-    catch{
+  
 
-    }
-    try{
-        if (password == /[!@#$%^&*(),.?":{}|<>]/g){
-        }
-    }
-    catch{
-
-    }
-    try {
-        if (password.lengh < 8){
-
-        }
-    }
-    catch{
-        
-    }
-    try {
-        if (password == null) {
-        }
-    }
-    catch{    
-    }
-}
+console.log(password_is_valid("Allie95@mok"))
