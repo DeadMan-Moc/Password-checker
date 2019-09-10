c109bb8b1c7c5e56553f2fd37051d4b49bbe56b9
DeadMan-MOC <mokgohloallie@gmail.com>
2019-09-10 09:51:30 +0200

    --- Auto-Git Commit ---

--- spec/passwordCheckerSpec.js
+++ spec/passwordCheckerSpec.js
@@ -7,7 +7,7 @@
 // })
 describe("Password validator", ()=>{
     it ("Checks for strong password.", ()=> {
-        var raw = "123OOPs$$5";
+        var raw = "Allnbd95";
         var answer = password_is_valid(raw);
         expect(answer).toBe(true);
     })
--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -1,28 +1,30 @@
+function password_is_ok(password){
+    try {
+        if (password !== password.match(/\d+/) ){
+              throw new Error("password should include 1 number")
+         }
+         if (password.match(/[!@#$%^&*-_(),.?":{}|<>]/g) ){
+             throw new Error("password should include 1 special character")
+         }
+         if (password.length !== 0) {
+             throw new Error("password cannot be empty")
+         }
+         if (password.length < 8){
+              throw new Error("password should be longer than 8 characters")
+         } 
+     }
+     catch(err){
+         throw new Error("Error "+err);
+     }
+ 
+}
+
 function password_is_valid(password)
 {
-//     try {
-//        if (password !== password.match(/\d+/) ){
-//             // throw new Error("password should include 1 number")
-//         }
-//         if (password.match(/[!@#$%^&*-_(),.?":{}|<>]/g) ){
-//             // throw new Error("password should include 1 special character")
-//         }
-//         if (password.length !== 0) {
-//             // throw new Error("password cannot be empty")
-//         }
-//         if (password.length < 8){
-//             // throw new Error("password should be longer than 8 characters")
-//         } 
-//     }
-//     catch(err){
-//         throw new Error("Error "+err);
-//     }
-// }
-
-        if (password.length != 0 && password.length >= 8)
+    if (*)
+        if (password_is_ok(password))
         {
-            if (password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])/)) {
-                // throw ("password is valid");
+            if (password.match(password_is_ok(password))) {
                 return true;
             }
             else {
@@ -35,4 +37,4 @@ function password_is_valid(password)
     }
   
 
-console.log(password_is_valid("Allie95@mok"));
+console.log(password_is_valid("Allie95mok"));
