9471f35bfc6d54f4fa6dcee2fa966119f46ff89b
DeadMan-MOC <mokgohloallie@gmail.com>
2019-09-10 10:21:32 +0200

    --- Auto-Git Commit ---

--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -1,40 +1,39 @@
 function password_is_ok(password){
     try {
-        if (password !== password.match(/\d+/) ){
+        if (password !== password.match(/[0-9]/) ){
               throw new Error("password should include 1 number")
-         }
-         if (password.match(/[!@#$%^&*-_(),.?":{}|<>]/g) ){
-             throw new Error("password should include 1 special character")
-         }
-         if (password.length !== 0) {
+        }
+        else if (password.length !== 0) {
              throw new Error("password cannot be empty")
-         }
-         if (password.length < 8){
+        }
+        else if (password.length < 8){
               throw new Error("password should be longer than 8 characters")
-         } 
+        } 
+        else {
+            return err
+        }
      }
-     catch(err){
-         throw new Error("Error "+err);
-     }
- 
+  
+        catch(err){
+         throw new Error(err);
+         }
 }
 
 function password_is_valid(password)
 {
-    if (*)
-        if (password_is_ok(password))
-        {
-            if (password.match(password_is_ok(password))) {
-                return true;
-            }
-            else {
-                throw new Error("password is invalid indeed.");
-            }
+    if (password.length >= 8 && password.length !== null)
+    {
+        if (password.match(password_is_ok(password))) {
+            return true;
         }
         else {
-            throw new Error("password is invalid");
+            throw new Error("password is invalid indeed.");
         }
     }
+    else {
+        throw new Error("password is invalid");
+    }
+}
   
 
 console.log(password_is_valid("Allie95mok"));
