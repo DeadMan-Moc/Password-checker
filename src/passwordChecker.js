8bf24e0a5b5d3bfc9486beeac438f03cb6fbc82d
DeadMan-MOC <mokgohloallie@gmail.com>
2019-09-10 14:51:39 +0200

    --- Auto-Git Commit ---

--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -9,17 +9,32 @@ function password_is_ok(password){
         return true;
     }
     else {
-        throw new Error("Password incorrect check conditions of the password");
+        return false;
     }
+        
 }
 function password_is_valid(password)
 {
-    if (password_is_ok(password)) {
-        return true;
-    }
-    else {
-        return false;
-    }
 
+    var condition2 = /(?=.{8,})/;
+    var condition3 = /[A-Z]/;
+    var condition4 = /[a-z]/;
+    var condition1 = /[0-9]/;
+    let combine = condition1 + condition2 + condition3 + condition4;
+    // let condition2 = /^[0-9]*$/g; 
+    // let condition3 = /^[a-z]*$/g;
+    // let condition4 = /^[A-Z]*$/g;
+    //let x = password.length > 8 && password.match(condition2) && password.match(condition3) && password.match(condition4);
+    //let y = condition2 && condition3 && condition4 && condition1;
+    let x = password.test(combine);
+    // console.log(y)
+    try {
+        if (x == false)
+            throw new Error ("passshjvbhk");
+    }
+    catch(e){
+        throw new Error("Error " +e);
+    }
+    return x;
 }
 console.log(password_is_valid("hhcJHh88hyuyu"));
