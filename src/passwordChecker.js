ce70c603b91c5667ed527c362e44bff70ea33b6f
DeadMan-MOC <mokgohloallie@gmail.com>
2019-09-10 10:51:42 +0200

    --- Auto-Git Commit ---

--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -1,39 +1,171 @@
 function password_is_ok(password){
-    try {
-        if (password !== password.match(/[0-9]/) ){
-              throw new Error("password should include 1 number")
-        }
-        else if (password.length !== 0) {
-             throw new Error("password cannot be empty")
-        }
-        else if (password.length < 8){
-              throw new Error("password should be longer than 8 characters")
-        } 
-        else {
-            return err
-        }
-     }
-  
-        catch(err){
-         throw new Error(err);
-         }
-}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    let condition2 = /[0-9]/; 
+    let condition3 = /[a-z]/;
+    let condition4 = /[A-Z]/;
+
+    if (password.length < 8 && password.match(condition2) && password.match(condition3) || password.match(condition4)){
+        return true;
+    }
+    else {
+        throw new Error("Password incorrect check conditions of the password");
+    }
 function password_is_valid(password)
 {
-    if (password.length >= 8 && password.length !== null)
-    {
-        if (password.match(password_is_ok(password))) {
-            return true;
-        }
-        else {
-            throw new Error("password is invalid indeed.");
-        }
+    if (password.match(password_is_ok(password))) {
+        return true;
     }
     else {
         throw new Error("password is invalid");
     }
+    
 }
-  
-
-console.log(password_is_valid("Allie95mok"));
+// console.log(password_is_valid("Allie95mok"));
