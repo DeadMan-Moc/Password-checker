9bc821d540a9b0f018e2b9d75198970812a88c28
DeadMan-MOC <mokgohloallie@gmail.com>
2019-09-10 11:23:30 +0200

    Src and spec done

--- spec/passwordCheckerSpec.js
+++ spec/passwordCheckerSpec.js
@@ -7,7 +7,7 @@
 // })
 describe("Password validator", ()=>{
     it ("Checks for strong password.", ()=> {
-        var raw = "Allnbd95";
+        var raw = "Allnssbd95";
         var answer = password_is_valid(raw);
         expect(answer).toBe(true);
     })
--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -1,171 +1,25 @@
 function password_is_ok(password){
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     
-    let condition2 = /[0-9]/; 
-    let condition3 = /[a-z]/;
-    let condition4 = /[A-Z]/;
+    let condition2 = /[0-9]/g; 
+    let condition3 = /[a-z]/g;
+    let condition4 = /[A-Z]/g;
 
-    if (password.length < 8 && password.match(condition2) && password.match(condition3) || password.match(condition4)){
+    if (password.length > 8 && password.match(condition2) && password.match(condition3) || password.match(condition4)){
         return true;
     }
     else {
         throw new Error("Password incorrect check conditions of the password");
     }
+}
 function password_is_valid(password)
 {
-    if (password.match(password_is_ok(password))) {
+    if (password_is_ok(password)) {
         return true;
     }
     else {
-        throw new Error("password is invalid");
+        return false;
     }
-    
+
 }
-// console.log(password_is_valid("Allie95mok"));
+console.log(password_is_valid("Allie95mok"));
